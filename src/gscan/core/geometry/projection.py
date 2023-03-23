8350a633d05993f64cb64fcd99ac2c94458f55fc
gralerfics <glverfer@outlook.com>
2023-03-23 17:03:38 +0800

    feat(main): add the testbench with mouse control.

--- src/gscan/core/geometry/projection.py
+++ src/gscan/core/geometry/projection.py
@@ -31,9 +31,25 @@ def calc_z_inscale(p):
     return np.array([u, v, w, 1])
 
 
+def calc_P_inscale(K, p):
+    # [ Description ]
+    #       Calculate the 3-D points (in scale)
+    # [ Arguments ]
+    #       K:          the intrinsic matrix of the camera
+    #       p:          the pixel coordinates
+    #           - format: [[u1, u2, u3, u4], [v1, v2, v3, v4]]
+    # [ Return ]
+    #       The coordinates of the four corners in the real scene: P_1, P_2, P_3 and P_4 (in scale)
+
+    z_scale = calc_z_inscale(p)
+    K_inv = np.linalg.inv(K)
+    p_homo = np.append(p, [1, 1, 1, 1]).reshape(3, -1)
+    return (np.matmul(K_inv, p_homo) * z_scale).swapaxes(0, 1)
+
+
 def calc_real_rect_ratio(K, p):
     # [ Description ]
-    #       Calculate
+    #       Calculate the ratio of the rectangle area in the real scene
     # [ Arguments ]
     #       K:          the intrinsic matrix of the camera
     #       p:          the pixel coordinates
@@ -41,9 +57,6 @@ def calc_real_rect_ratio(K, p):
     # [ Return ]
     #       The value of l_21 / l_23 (clockwise, and 1 is the left-top point)
 
-    z_scale = calc_z_inscale(p)
-    K_inv = np.linalg.inv(K)
-    p_homo = np.append(p, [1, 1, 1, 1]).reshape(3, -1)
-    P1, P2, P3, P4 = (np.matmul(K_inv, p_homo) * z_scale).swapaxes(0, 1)
+    P1, P2, P3, P4 = calc_P_inscale(K, p)
     return np.linalg.norm(P1 - P2) / np.linalg.norm(P3 - P2)
 
--- src/main.py
+++ src/main.py
@@ -6,31 +6,59 @@ from gscan.core.geometry import projection
 from gscan.core.basic.regulator import ImageValueRegulator
 
 
+# mouse control
+picked = 0
+pixel_x = []
+pixel_y = []
+
+def mouse_handler(event, x, y, flags, param):
+    global picked
+    if event == cv2.EVENT_LBUTTONDOWN and picked < 4:
+        picked += 1
+        pixel_x.append(x)
+        pixel_y.append(y)
+        cv2.circle(img_show, (x, y), 2, (0, 0, 255), 2)
+        if picked > 1:
+            cv2.line(img_show, (pixel_x[picked - 2], pixel_y[picked - 2]), (x, y), (255, 0, 0), 2)
+        if picked == 4:
+            cv2.line(img_show, (x, y), (pixel_x[0], pixel_y[0]), (255, 0, 0), 2)
+        cv2.imshow("selecting", img_show)
+
+
+# parameters
 K = np.array([[1536.1, 0, 959.5], [0, 1535.7, 723.8], [0, 0, 1]])
+target_height = 500
 
-p2 = np.array([[698, 961, 981, 740],
-               [431, 455, 723, 684]])
 
-ratio = projection.calc_real_rect_ratio(K, p2)
-p1_h = 500
-p1_w = int(p1_h * ratio)
-p1 = np.array([[0, p1_w, p1_w, 0],
-               [0, 0, p1_h, p1_h]])
+# load image
+img_raw = cv2.imread("../res/test_3.jpg", cv2.IMREAD_COLOR)
 
-c1, c2, c3, c4 = p2.swapaxes(0, 1).tolist()
 
-img = cv2.imread("../res/test_3.jpg", cv2.IMREAD_COLOR)
-img_show = np.array(img)
+# selecting
+img_show = np.array(img_raw)
+cv2.namedWindow("selecting")
+cv2.imshow("selecting", img_show)
+cv2.setMouseCallback("selecting", mouse_handler)
+while picked < 4:
+    cv2.waitKey(1)
+p_uv = np.array([pixel_x, pixel_y])
+
+c1, c2, c3, c4 = p_uv.swapaxes(0, 1).tolist()
 cv2.line(img_show, c1, c2, (0, 0, 255), 2)
 cv2.line(img_show, c2, c3, (0, 0, 255), 2)
 cv2.line(img_show, c3, c4, (0, 0, 255), 2)
 cv2.line(img_show, c4, c1, (0, 0, 255), 2)
-cv2.imshow("raw", img_show)
-cv2.waitKey(0)
 
-H = homography.get_homography_matrix(p1, p2)
-res = homography.homography_correction(img, H, (p1_h, p1_w), regulator=ImageValueRegulator)
 
+# scale calculation
+ratio = projection.calc_real_rect_ratio(K, p_uv)
+target_width = int(target_height * ratio)
+p_target = np.array([[0, target_width, target_width, 0], [0, 0, target_height, target_height]])
+
+
+# homography correction
+H = homography.get_homography_matrix(p_target, p_uv)
+res = homography.homography_correction(img_raw, H, (target_height, target_width), regulator=ImageValueRegulator)
 cv2.imshow("corrected", res)
 cv2.waitKey(0)
 
